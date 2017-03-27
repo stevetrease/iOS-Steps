fb6bec82eeb2b9aff9cf109d26463d60f1024efc
Steve <steve@broadband.bt.com>
2017-03-27 12:43:26 +0100

    First attempt at adding actual step counts to today extension

--- iOS Steps Today/TodayViewController.swift
+++ iOS Steps Today/TodayViewController.swift
@@ -8,15 +8,20 @@
 
 import UIKit
 import NotificationCenter
+import HealthKit
 
 class TodayViewController: UIViewController, NCWidgetProviding {
     
-    @IBOutlet weak var textLabel: UILabel!
-
+    let healthStore = HKHealthStore()
+    var stepsToday: Double = 0.0
+    
+    @IBOutlet weak var stepsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print ("viewDidLoad")
-        // Do any additional setup after loading the view from its nib.
+        
+        checkHealthKitAuthorization()
         
         updateView ()
     }
@@ -33,6 +38,7 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // Dispose of any resources that can be recreated.
     }
     
+    /*
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
         print ("widgetPerformUpdate")
@@ -45,14 +51,88 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         }
         
         completionHandler(NCUpdateResult.newData)
-    }
+    } */
     
     
     func updateView () {
-        let numberFormatter = NumberFormatter()
-        let numberString = numberFormatter.string(from: arc4random_uniform(10000) + 1 as NSNumber)
-        textLabel.text = numberString
-        print (numberString!)
+        // let numberFormatter = NumberFormatter()
+        // let numberString = numberFormatter.string(from: arc4random_uniform(10000) + 1 as NSNumber)
+        //textLabel.text = numberString
+        //print (numberString!)
+    
+        getTodayStepCount (completion: { (steps) in
+            print ("getTodayStepCount")
+            let numberFormatter = NumberFormatter()
+            let numberString = numberFormatter.string(from: steps! as NSNumber)
+            print ("numberString ", numberString!)
+
+            
+            OperationQueue.main.addOperation {
+                self.stepsLabel.text = numberString!
+            }
+        })
+
+        
     }
+    
+    
+    
+    func checkHealthKitAuthorization() ->()
+    {
+        // Default to assuming that we're authorized
+        var isHealthKitEnabled = true
+        
+        // Do we have access to HealthKit on this device?
+        if HKHealthStore.isHealthDataAvailable() {
+            print ("healthKit is available")
+            
+            let healthKitTypesToRead : Set = [
+                HKObjectType.quantityType(forIdentifier:HKQuantityTypeIdentifier.stepCount)!
+            ]
+            healthStore.requestAuthorization(toShare: nil, read: healthKitTypesToRead) { (success, error) -> Void in
+                if (error != nil) {
+                    isHealthKitEnabled = true
+                } else {
+                    isHealthKitEnabled = false
+                }
+            }
+        } else {
+            isHealthKitEnabled = false
+        }
+        print (isHealthKitEnabled)
+    }
+    
+    func getTodayStepCount(completion:@escaping (Double?)->())
+    {
+        
+        //   Define the sample type
+        let type = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
+        
+        let cal = Calendar(identifier: .gregorian)
+        let startDate = cal.startOfDay(for: Date())
+        let endDate = Date()
+        
+        //  Set the predicate
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+        
+        let query = HKStatisticsQuery(quantityType: type!, quantitySamplePredicate: predicate, options: .cumulativeSum) { query, results, error in
+            print ("query: getTodayStepCount")
+            let quantity = results?.sumQuantity()
+            let unit = HKUnit.count()
+            let steps = quantity?.doubleValue(for: unit)
+            
+            if steps != nil {
+                self.stepsToday = steps!
+                completion(steps)
+            } else {
+                print("getTodayStepCount: results are nil - returning zero steps")
+                self.stepsToday = 0.0
+                completion(0.0)
+            }
+        }
+        healthStore.execute(query)
+    }
+
+
     
 }
