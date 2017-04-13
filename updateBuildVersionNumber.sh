78820194034460a4bea7721cee4b43ce970da386
Steve <steve@broadband.bt.com>
2017-04-13 16:25:17 +0100

    changed updateVersion number, etc to updateBuild, etc....

--- updateBuildVersionNumber.sh
+++ updateBuildVersionNumber.sh
@@ -1,15 +1,15 @@
 /bin/sh
 
-LAST_VERSION=`cat ./version`
-NEW_VERSION=$(($LAST_VERSION + 1))
+LAST_BUILD=`cat ./build`
+NEW_BUILD=$(($LAST_BUILD + 1))
 
-echo $NEW_VERSION > version
-echo "version:" $NEW_VERSION
+echo $NEW_BUILD > build
+echo "build:" $NEW_BUILD
 
 PLB=/usr/libexec/PlistBuddy
 
 for DELIVERABLE in "iOS Steps" "iOS Steps Today" "iOS Steps Watch" "iOS Steps Watch Extension"
 do
 	PLIST="$DELIVERABLE/Info.plist"
-	$PLB -c "Set :CFBundleVersion $NEW_VERSION" "$PLIST"
+	$PLB -c "Set :CFBundleVersion $NEW_BUILD" "$PLIST"
 done
