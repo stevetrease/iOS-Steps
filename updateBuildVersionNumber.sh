e7c34a9f55fcb6163ff607fd476b0b8459f6cdf2
Steve <steve@carbon.local>
2017-04-13 16:23:36 +0100

    removed crud from updateBuildVersionNumber.sh

--- updateBuildVersionNumber.sh
+++ updateBuildVersionNumber.sh
@@ -10,18 +10,6 @@ PLB=/usr/libexec/PlistBuddy
 
 for DELIVERABLE in "iOS Steps" "iOS Steps Today" "iOS Steps Watch" "iOS Steps Watch Extension"
 do
-	echo $PLIST
 	PLIST="$DELIVERABLE/Info.plist"
 	$PLB -c "Set :CFBundleVersion $NEW_VERSION" "$PLIST"
 done
-echo
-exit
-
-PLIST="${PROJECT_DIR}/${INFOPLIST_FILE}"
-PLB=/usr/libexec/PlistBuddy
-
-LAST_VERSION=$($PLB -c "Print CFBundleVersion" "$PLIST")
-NEW_VERSION=$(($LAST_VERSION + 1))
-
-exit
-$PLB -c "Set :CFBundleVersion $NEW_VERSION" "$PLIST")
