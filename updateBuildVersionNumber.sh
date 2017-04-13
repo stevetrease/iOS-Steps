7056c95891475b59a8401d990875abaf05e40d8c
Steve <steve@broadband.bt.com>
2017-04-13 16:23:00 +0100

    Automated build number into all Info.plists

--- updateBuildVersionNumber.sh
+++ updateBuildVersionNumber.sh
@@ -1,20 +1,27 @@
 /bin/sh
 
-LAST_NUMBER=`cat ./version`
-NEW_NUMBER=$(($LAST_NUMBER + 1))
+LAST_VERSION=`cat ./version`
+NEW_VERSION=$(($LAST_VERSION + 1))
 
-echo $NEW_NUMBER > version
+echo $NEW_VERSION > version
+echo "version:" $NEW_VERSION
 
 PLB=/usr/libexec/PlistBuddy
-for FILE in 
 
+for DELIVERABLE in "iOS Steps" "iOS Steps Today" "iOS Steps Watch" "iOS Steps Watch Extension"
+do
+	echo $PLIST
+	PLIST="$DELIVERABLE/Info.plist"
+	$PLB -c "Set :CFBundleVersion $NEW_VERSION" "$PLIST"
+done
+echo
 exit
 
 PLIST="${PROJECT_DIR}/${INFOPLIST_FILE}"
 PLB=/usr/libexec/PlistBuddy
 
-LAST_NUMBER=$($PLB -c "Print CFBundleVersion" "$PLIST")
-NEW_VERSION=$(($LAST_NUMBER + 1))
+LAST_VERSION=$($PLB -c "Print CFBundleVersion" "$PLIST")
+NEW_VERSION=$(($LAST_VERSION + 1))
 
 exit
 $PLB -c "Set :CFBundleVersion $NEW_VERSION" "$PLIST")
