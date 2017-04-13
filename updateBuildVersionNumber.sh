debdc0c56bff11367bf6d6deb0e64dd8991b2785
Steve <steve@broadband.bt.com>
2017-04-13 07:04:21 +0100

    changes to version update script

--- updateBuildVersionNumber.sh
+++ updateBuildVersionNumber.sh
@@ -1,10 +1,20 @@
 /bin/sh
 
-echo ${PROJECT_DIR} > ~steve/Desktop/text.txt
+LAST_NUMBER=`cat ./version`
+NEW_NUMBER=$(($LAST_NUMBER + 1))
+
+echo $NEW_NUMBER > version
+
+PLB=/usr/libexec/PlistBuddy
+for FILE in 
+
 exit
 
 PLIST="${PROJECT_DIR}/${INFOPLIST_FILE}"
 PLB=/usr/libexec/PlistBuddy
+
 LAST_NUMBER=$($PLB -c "Print CFBundleVersion" "$PLIST")
 NEW_VERSION=$(($LAST_NUMBER + 1))
+
+exit
 $PLB -c "Set :CFBundleVersion $NEW_VERSION" "$PLIST")
