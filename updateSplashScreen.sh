c3fbbcef22be56d5103ba84e0a7d34c565312eaa
stevetrease <steve@broadband.bt.com>
2017-05-05 20:32:51 +0100

    Added short git has to version string on launch screen

--- updateSplashScreen.sh
+++ updateSplashScreen.sh
@@ -3,10 +3,11 @@
 LAST_BUILD=0
 LAST_BUILD=`cat ./build`
 NEW_BUILD=$(($LAST_BUILD + 1))
+GIT=`git rev-parse --short HEAD`
 
 echo $NEW_BUILD > build
-echo "build:" $NEW_BUILD
+echo "build:" $NEW_BUILD $GIT
 
 PLB=/usr/libexec/PlistBuddy
 
-sed -i bak -e "/userLabel=\"APP_VERSION\"/s/text=\"[^\"]*\"/text=\"Build: $NEW_BUILD\"/" "iOS Steps"/Base.lproj/LaunchScreen.storyboard
+sed -i bak -e "/userLabel=\"APP_VERSION\"/s/text=\"[^\"]*\"/text=\"Build: $NEW_BUILD \($GIT\)\"/" "iOS Steps"/Base.lproj/LaunchScreen.storyboard
