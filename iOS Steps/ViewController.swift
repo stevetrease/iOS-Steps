5c27a3aad068d9dac7bd2c5eb4ded0eae4635e9d
stevetrease <steve@carbon.local>
2017-05-06 14:49:28 +0100

    Pulled chart drawing into a function.

--- iOS Steps/ViewController.swift
+++ iOS Steps/ViewController.swift
@@ -83,94 +83,99 @@ class ViewController: UIViewController {
             }
         })
         
-        
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
         healthKitManager.updateHourlyStepsArray(completion: { (steps) in
             OperationQueue.main.addOperation {
-                var hourlyDataEntries: [BarChartDataEntry] = []
-                var line1Data: [ChartDataEntry] = []
-                var line2Data: [ChartDataEntry] = []
-                
-                var firstHour = 24.0
-                var lastHour = 0.0
-                
-                var accumulator1 = 0.0
-                for i in 0..<healthKitManager.hourlySteps.count {
-                    let cal = Calendar.current
-                    let d = healthKitManager.hourlySteps[i].timeStamp
-                    let components = cal.dateComponents ([.hour], from: d)
-                    let hour = Double(components.hour!)
-                    let value = healthKitManager.hourlySteps[i].value
-                    
-                    if (hour < firstHour) {
-                        firstHour = hour
-                    }
-                    if (hour > lastHour) {
-                        lastHour = hour
-                    }
-                
-                    accumulator1 = accumulator1 + value
-                    
-                    let hourlyDataEntry = BarChartDataEntry(x: hour, y: value)
-                    hourlyDataEntries.append(hourlyDataEntry)
-                    
-                    let line1DataPoint = ChartDataEntry (x: hour, y: Double (accumulator1))
-                    line1Data.append (line1DataPoint)
-                }
-                
-                var accumulator2 = 0.0
-                for i in 0..<healthKitManager.hourlyStepsYesterday.count {
-                    let cal = Calendar.current
-                    let d = healthKitManager.hourlyStepsYesterday[i].timeStamp
-                    let components = cal.dateComponents ([.hour], from: d)
-                    let hour = Double(components.hour!)
-                    let value = healthKitManager.hourlyStepsYesterday[i].value
-                    
-                    if (hour < firstHour) {
-                        firstHour = hour
-                    }
-                    if (hour > lastHour) {
-                        
-                        lastHour = hour
-                    }
-                    
-                    accumulator2 = accumulator2 + value
-                    
-                    let line2DataPoint = ChartDataEntry (x: hour, y: Double (accumulator2))
-                    line2Data.append (line2DataPoint)
-                }
-                
-                let barDataSet = BarChartDataSet(values: hourlyDataEntries, label: "")
-                let lineDataSet1 = LineChartDataSet(values: line1Data, label: "Today")
-                let lineDataSet2 = LineChartDataSet(values: line2Data, label: "Yesterday")
-                
-                barDataSet.colors = [UIColor.darkText]
-                lineDataSet1.colors = [UIColor.darkText]
-                lineDataSet1.drawCirclesEnabled = false
-                lineDataSet1.lineWidth = 3
-                lineDataSet2.colors = [UIColor.lightGray]
-                lineDataSet2.drawCirclesEnabled = false
-                lineDataSet2.lineWidth = 2
-                
-                let barData = BarChartData(dataSets: [barDataSet])
-                let lineData = LineChartData (dataSets: [lineDataSet2, lineDataSet1])
-                
-                lineData.setDrawValues(false)
-                
-                let data: CombinedChartData = CombinedChartData()
-                data.barData = barData
-                data.lineData = lineData
-                
-                self.chartView.xAxis.axisMinimum = firstHour - 0.5
-                self.chartView.xAxis.axisMaximum = lastHour + 0.5
-
-                self.chartView.data = data
-                self.chartView.data?.notifyDataChanged()
-                self.chartView.notifyDataSetChanged()
-                
+                self.drawChart2()
                 UIApplication.shared.isNetworkActivityIndicatorVisible = false
-             }
+            }
         })
+    }
+    
+    
+    func drawChart2 () {
+        var hourlyDataEntries: [BarChartDataEntry] = []
+        var line1Data: [ChartDataEntry] = []
+        var line2Data: [ChartDataEntry] = []
+        
+        var firstHour = 24.0
+        var lastHour = 0.0
+        
+        var accumulator1 = 0.0
+        for i in 0..<healthKitManager.hourlySteps.count {
+            let cal = Calendar.current
+            let d = healthKitManager.hourlySteps[i].timeStamp
+            let components = cal.dateComponents ([.hour, .minute], from: d)
+            let hour = Double(components.hour!)
+            let minutes = Double(components.hour!)
+            let value = healthKitManager.hourlySteps[i].value
+            
+            if (hour < firstHour) {
+                firstHour = hour
+            }
+            if (hour > lastHour) {
+                lastHour = hour
+            }
+            
+            accumulator1 = accumulator1 + value
+            
+            let hourlyDataEntry = BarChartDataEntry(x: hour, y: value)
+            hourlyDataEntries.append(hourlyDataEntry)
+            
+            let line1DataPoint = ChartDataEntry (x: hour + (minutes / 60), y: Double (accumulator1))
+            line1Data.append (line1DataPoint)
+        }
+        
+        var accumulator2 = 0.0
+        for i in 0..<healthKitManager.hourlyStepsYesterday.count {
+            let cal = Calendar.current
+            let d = healthKitManager.hourlyStepsYesterday[i].timeStamp
+            let components = cal.dateComponents ([.hour, .minute], from: d)
+            let hour = Double(components.hour!)
+            let minutes = Double(components.hour!)
+            let value = healthKitManager.hourlyStepsYesterday[i].value
+            
+            if (hour < firstHour) {
+                firstHour = hour
+            }
+            if (hour > lastHour) {
+                
+                lastHour = hour
+            }
+            
+            accumulator2 = accumulator2 + value
+            
+            let line2DataPoint = ChartDataEntry (x: hour + (minutes / 60), y: Double (accumulator2))
+            line2Data.append (line2DataPoint)
+        }
+        
+        let barDataSet = BarChartDataSet(values: hourlyDataEntries, label: "")
+        let lineDataSet1 = LineChartDataSet(values: line1Data, label: "Today")
+        let lineDataSet2 = LineChartDataSet(values: line2Data, label: "Yesterday")
+        
+        barDataSet.colors = [UIColor.darkText]
+        lineDataSet1.colors = [UIColor.darkText]
+        lineDataSet1.drawCirclesEnabled = false
+        lineDataSet1.lineWidth = 3
+        lineDataSet2.colors = [UIColor.lightGray]
+        lineDataSet2.drawCirclesEnabled = false
+        lineDataSet2.lineWidth = 2
+        
+        let barData = BarChartData(dataSets: [barDataSet])
+        let lineData = LineChartData (dataSets: [lineDataSet2, lineDataSet1])
+        
+        lineData.setDrawValues(false)
+        
+        let data: CombinedChartData = CombinedChartData()
+        data.barData = barData
+        data.lineData = lineData
+        
+        self.chartView.xAxis.axisMinimum = firstHour - 0.5
+        self.chartView.xAxis.axisMaximum = lastHour + 0.5
+        
+        self.chartView.data = data
+        self.chartView.data?.notifyDataChanged()
+        self.chartView.notifyDataSetChanged()
     }
     
     
