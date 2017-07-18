40fb49beba69a888109b58c578f94c0f9b9cc350
Steve Trease <steve.trease@shell.com>
2017-07-18 11:39:36 +0100

    Added display of yesterday's steps to iOS app

--- iOS Steps Watch Extension/InterfaceController.swift
+++ iOS Steps Watch Extension/InterfaceController.swift
@@ -16,9 +16,11 @@ class InterfaceController: WKInterfaceController {
 
     @IBOutlet var stepsLabel: WKInterfaceLabel!
     @IBOutlet var averageLabel: WKInterfaceLabel!
+    @IBOutlet var yesterdayLabel: WKInterfaceLabel!
     
     
     var stepsToday: Double = 0.0
+    var stepsYesterday: Double = 0.0
     var sevenDayStepAverage: Double = 100000.0
     var sevenDayStepAverageLastUpdated: Date = Date.distantPast
     
@@ -111,6 +113,17 @@ class InterfaceController: WKInterfaceController {
                     }
                 }
             })
+            
+            getYesterdayStepCount (completion: { (steps) in
+                if steps != -1.0 {
+                    self.stepsYesterday = steps!
+                    OperationQueue.main.addOperation {
+                        let numberString = self.numberFormatter.string(from: steps! as NSNumber)
+                        self.yesterdayLabel.setText(numberString!)
+                   }
+                }
+            })
+            
         }
     }
 
@@ -123,6 +136,34 @@ class InterfaceController: WKInterfaceController {
         let cal = Calendar.current
         let startDate = cal.startOfDay(for: Date())
         let endDate = Date()
+        
+        //  Set the predicate
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
+        
+        let query = HKStatisticsQuery(quantityType: type!, quantitySamplePredicate: predicate, options: .cumulativeSum) { query, results, error in
+            let quantity = results?.sumQuantity()
+            let unit = HKUnit.count()
+            let steps = quantity?.doubleValue(for: unit)
+            
+            if steps != nil {
+                completion(steps)
+            } else {
+                print("getTodayStepCount: results are nil - returning nil steps")
+                completion(-1.0)
+            }
+        }
+        healthStore.execute(query)
+    }
+    
+    
+    func getYesterdayStepCount(completion:@escaping (Double?)->())
+    {
+        //   Define the sample type
+        let type = HKQuantityType.quantityType(forIdentifier: HKQuantityTypeIdentifier.stepCount)
+        
+        let cal = Calendar.current
+        let endDate = cal.startOfDay(for: Date())
+        let startDate =  cal.date(byAdding: .day, value: -1, to: endDate)
         
         //  Set the predicate
         let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: [])
