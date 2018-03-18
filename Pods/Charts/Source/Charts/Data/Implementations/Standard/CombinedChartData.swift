c90c9e53a16fc0e14ef7c0132fcc4fdc604ed7b7
Steve <stevetrease@gmail.com>
2018-03-18 18:28:15 +0000

    Charts pod upgrade to 3.0.5.

--- Pods/Charts/Source/Charts/Data/Implementations/Standard/CombinedChartData.swift
+++ Pods/Charts/Source/Charts/Data/Implementations/Standard/CombinedChartData.swift
@@ -13,11 +13,11 @@ import Foundation
 
 open class CombinedChartData: BarLineScatterCandleBubbleChartData
 {
-    fileprivate var _lineData: LineChartData!
-    fileprivate var _barData: BarChartData!
-    fileprivate var _scatterData: ScatterChartData!
-    fileprivate var _candleData: CandleChartData!
-    fileprivate var _bubbleData: BubbleChartData!
+    private var _lineData: LineChartData!
+    private var _barData: BarChartData!
+    private var _scatterData: ScatterChartData!
+    private var _candleData: CandleChartData!
+    private var _bubbleData: BubbleChartData!
     
     public override init()
     {
@@ -29,7 +29,7 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
         super.init(dataSets: dataSets)
     }
     
-    open var lineData: LineChartData!
+    @objc open var lineData: LineChartData!
     {
         get
         {
@@ -42,7 +42,7 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
         }
     }
     
-    open var barData: BarChartData!
+    @objc open var barData: BarChartData!
     {
         get
         {
@@ -55,7 +55,7 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
         }
     }
     
-    open var scatterData: ScatterChartData!
+    @objc open var scatterData: ScatterChartData!
     {
         get
         {
@@ -68,7 +68,7 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
         }
     }
     
-    open var candleData: CandleChartData!
+    @objc open var candleData: CandleChartData!
     {
         get
         {
@@ -81,7 +81,7 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
         }
     }
     
-    open var bubbleData: BubbleChartData!
+    @objc open var bubbleData: BubbleChartData!
     {
         get
         {
@@ -136,31 +136,37 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
             {
                 _xMin = data.xMin
             }
-            
-            if data.yMax > _leftAxisMax
+
+            for dataset in sets
             {
-                _leftAxisMax = data.yMax
-            }
-            
-            if data.yMin < _leftAxisMin
-            {
-                _leftAxisMin = data.yMin
-            }
-            
-            if data.yMax > _rightAxisMax
-            {
-                _rightAxisMax = data.yMax
-            }
-            
-            if data.yMin < _rightAxisMin
-            {
-                _rightAxisMin = data.yMin
+                if dataset.axisDependency == .left
+                {
+                    if dataset.yMax > _leftAxisMax
+                    {
+                        _leftAxisMax = dataset.yMax
+                    }
+                    if dataset.yMin < _leftAxisMin
+                    {
+                        _leftAxisMin = dataset.yMin
+                    }
+                }
+                else
+                {
+                    if dataset.yMax > _rightAxisMax
+                    {
+                        _rightAxisMax = dataset.yMax
+                    }
+                    if dataset.yMin < _rightAxisMin
+                    {
+                        _rightAxisMin = dataset.yMin
+                    }
+                }
             }
         }
     }
     
     /// - returns: All data objects in row: line-bar-scatter-candle-bubble if not null.
-    open var allData: [ChartData]
+    @objc open var allData: [ChartData]
     {
         var data = [ChartData]()
         
@@ -188,7 +194,7 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
         return data
     }
     
-    open func dataByIndex(_ index: Int) -> ChartData
+    @objc open func dataByIndex(_ index: Int) -> ChartData
     {
         return allData[index]
     }
@@ -278,26 +284,24 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
         {
             return nil
         }
-        else
+        
+        // The value of the highlighted entry could be NaN - if we are not interested in highlighting a specific value.
+        let entries = data.getDataSetByIndex(highlight.dataSetIndex).entriesForXValue(highlight.x)
+        for e in entries
         {
-            // The value of the highlighted entry could be NaN - if we are not interested in highlighting a specific value.
-            let entries = data.getDataSetByIndex(highlight.dataSetIndex).entriesForXValue(highlight.x)
-            for e in entries
+            if e.y == highlight.y || highlight.y.isNaN
             {
-                if e.y == highlight.y || highlight.y.isNaN
-                {
-                    return e
-                }
+                return e
             }
-            return nil
         }
+        return nil
     }
     
-    /// get data set for highlight
+    /// Get dataset for highlight
     ///
     /// - Parameter highlight: current highlight
     /// - Returns: dataset related to highlight
-    open func getDataSetByHighlight(_ highlight: Highlight) -> IChartDataSet!
+    @objc open func getDataSetByHighlight(_ highlight: Highlight) -> IChartDataSet!
     {  
         if highlight.dataIndex >= allData.count
         {
@@ -310,9 +314,7 @@ open class CombinedChartData: BarLineScatterCandleBubbleChartData
         {
             return nil
         }
-        else
-        {
-            return data.dataSets[highlight.dataSetIndex]
-        }
+        
+        return data.dataSets[highlight.dataSetIndex]
     }
 }
